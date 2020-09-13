50ed80e07b3593ed0eb3756c9df2028af54c785b
Andres X. Vargas <axvargas@fiec.espol.edu.ec>
2020-09-13 15:04:54 -0500

    Database config done

--- config/database.js
+++ config/database.js
@@ -4,15 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'mysql',
+        client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'real_estate'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'nk8in78f'),
-        ssl: env.bool('DATABASE_SSL', false),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
-      options: {}
+      options: {
+        ssl: false,
+      },
     },
   },
 });
